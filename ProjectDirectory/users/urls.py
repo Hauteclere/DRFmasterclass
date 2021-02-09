94713ef5c2b1bfd53a0da96111ee7bb83bab72b9
Hauteclere <oliver.f.lavers@gmail.com>
2021-02-10 05:14:16 +1000

    wiped the project directory ready for live runthrough

--- ProjectDirectory/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('users/', views.UserList.as_view()),
-    path('my-profile/', views.UserDetail.as_view()),
-    path('new-team/', views.AddTeam.as_view()),
-]
